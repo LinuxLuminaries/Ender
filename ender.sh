18d6b5f154d640d07f13e23549ad0a626fecbdf3
Linux Luminaries <adityasir.n2578@gmail.com>
2023-10-07 00:04:51 +0530

    Update ender.sh
    added CTRL+C catch block and little modification in the output style

--- ender.sh
+++ ender.sh
@@ -1,40 +1,63 @@
 #!/bin/bash
+
+#Catching CTRL+C
+cleanup() {
+	sleep 1
+	echo ""
+    echo -e "[${BLUE}${INFO}${RESET}]Exiting."
+    # Add any cleanup tasks here
+    exit 0
+}
+
+# Set up a trap to catch the Ctrl+C signal (SIGINT)
+trap cleanup INT
+
 GREEN="\033[0;32m"
 RESET="\033[0m"
 RED="\033[0;31m"
 BLUE="\033[0;34m"
 TICK="\e[32m\u2713\e[0m"
-FAIL="\e[31m\u2717\e[0m"
+CROSS="\e[31m\u2717\e[0m"
+INFO="\x21"
 
+# If no argument is passed
 if [ $# -eq 0 ]; then
-	echo -e "Usage: $0 [${BLUE}+${RESET}]website[${BLUE}+${RESET}]"
+	echo "Usage: $0 [${BLUE}+${RESET}]website[${BLUE}+${RESET}]"
 	exit 1
 fi
+
 
 #Checking the internet connection
-echo -e "[${BLUE}+${RESET}]Checking the Internet Connection ..."
+echo -e "[${BLUE}+${RESET}]Checking the Internet Connection."
 ping 8.8.8.8 -c 1 > /dev/null
+
 #"$?" stores the status code of above line
 if [[ $"$?" -eq 1 ]]; then
-	echo -e "Internet Check Failed[${RED}${FAIL}${RESET}]"
+	echo -e "Internet Check Failed [${RED}${CROSS}${RESET}]"
 	exit 1
 else 
-	echo -e "Internet Check Done [${TICK}]${RESET}"
+	echo -e "Internet Check Done [${GREEN}${TICK}]${RESET}"
 fi
 
-url="$1"
-folder=trj_output
+#if http or https is present in domain
+if [[ "$1" == http* ]]; then
+	url=$(echo "$1" | grep -oP '(?<=://)([^/]+)' | sed 's/^www\.//')
+else
+	url=$1
+fi
+
+folder=ender_output
 domain=$(echo "$url" | cut -d'.' -f1)
 sleep 1
 
 # verify the domain
 echo -e "[${BLUE}+${RESET}]Checking the Domain."
-ping $url -c 1 >/dev/null
-if [[ $"$?" -eq 0 ]]; then
-	echo -e "Domain Check Done[${TICK}${RESET}]"
-else
-	echo -e "Domain Check Failed![${RED}${FAIL}${RESET}]"
-	exit 1
+response=$(curl -IsSL "$url" -o /dev/null -w "%{http_code}")
+
+    if [[ $response == 200 ]]; then
+        echo -e "$url is live [${GREEN}${TICK}${RESET}]"
+    else
+        echo -e "Domain $url is not live (HTTP response code: $response) [${RED}${CROSS}${RESET}]"
 fi
 
 # Specify the name of the tool
@@ -44,26 +67,26 @@ t2="gau"
 # Check if uro is installed
 pip list 2>/dev/null | grep uro &>/dev/null
 if [ $"$?" -eq 1 ]; then
-	echo -e "${RED}[+]${RESET}URO is not Installed... Please Install it and rerun this Script..."
+	echo -e "${RED}[+]${RESET} URO is not Installed... Please Install it and rerun this Script..."
 	exit 1
 fi
 
 if ! command -v "$t2"&>/dev/null; then
-    echo -e "${BLUE}[+]${RESET}$t2 is not installed. Installing $t2 ..."
+    echo -e "${BLUE}[+]${RESET} $t2 is not installed. Installing $t2 ..."
     wget -q https://github.com/lc/gau/releases/download/v2.1.2/gau_2.1.2_linux_amd64.tar.gz>/dev/null
     tar xvf gau_2.1.2_linux_amd64.tar.gz>/dev/null
-    echo -e "${BLUE}[+]${RESET}ENTER THE ROOT PASSWORD TO COPY ESSENTIAL FILES"
+    echo -e "${BLUE}[+]${RESET} ENTER THE ROOT PASSWORD TO COPY ESSENTIAL FILES"
     sudo mv gau /usr/bin/gau
     rm gau_2.1.2_linux_amd64.tar.gz
 fi
 if ! command -v "$t2"&>/dev/null; then
-	echo -e "[${RED}+${RESET}]Please Install GAU Manually!"
+	echo -e "[${RED}+${RESET}] Please Install GAU Manually!"
 fi
 sleep 2
-	#echo -e "[${GREEN}+${RESET}]Everything is Configured! Moving UP.."
-sleep 3
+
 # Run the echo command and store the output in params.txt
 echo -e "[${BLUE}+${RESET}]Gathering The End-Points for ${green}$url${RESET}"
+
 #Checking the Domain Folder Exists or not
 if [ ! -d $folder ]; then
 	mkdir $folder
@@ -78,7 +101,7 @@ cd $folder
 if [[ -f "${domain}.txt" ]]; then
         echo -e "[${GREEN}+${RESET}]EndPoints Gathered! Now Analyzing ${domain}.txt"
 	if [[ ! -s "${domain}.txt" ]]; then
-		echo -e "[${RED}${FAIL}${RESET}]No EndPoints Found"
+		echo -e "[${RED}${CROSS}${RESET}]No EndPoints Found"
 		echo -e "[${BLUE}${TICK}${RESET}Exiting"; sleep 1
 		exit -1
 	fi
